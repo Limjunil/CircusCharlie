bc2f916f324369c0a0de07d8307059ea1e168015
DESKTOP-052EG82\KGA_22 <im5820795@naver.com>
2023-02-01 17:15:08 +0900

    [Junil] Add bgImage copy

--- CircusCharlie/Assets/01.UnityProject/Scripts/Runtime/BgImageController.cs
+++ CircusCharlie/Assets/01.UnityProject/Scripts/Runtime/BgImageController.cs
@@ -1,21 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BgImageController : MonoBehaviour
 {
-
+    
     public bool leftMove = false;
     public bool rightMove = false;
 
-    public GameObject[] bgImage = default;
+    public GameObject bgImagePrefab;
+
+    // 생성할 배경화면 배열
+    private GameObject[] bgImage = default;
     private int bgImageCount = default;
+
+    private Vector3 bgImagePosition = default;
 
     // Start is called before the first frame update
     void Start()
     {
 
         bgImageCount = 5;
+
+        bgImagePosition = new Vector3(-3000f, 0f, 0f);
+
+        
+        bgImage = new GameObject[bgImageCount];
+
+        for (int i = 0; i < bgImageCount; i++)
+        {
+            bgImage[i] = Instantiate(bgImagePrefab, bgImagePosition, 
+                Quaternion.identity, gameObject.transform);
+
+        }
+
+        Vector3 bgImageLocalPos = new Vector3(-2400f, -200f, 0f);
+
+        for (int i = 0; i < bgImageCount; i++)
+        {
+            bgImage[i].transform.localPosition = bgImageLocalPos;
+
+            bgImageLocalPos.x += 1200f;
+        }
+
+        
     }
 
     // Update is called once per frame
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Make a CircusCharlie
 2023-01-30 / v0.0.1 / Init CircusCharlie game project   
 2023-02-01 / v0.0.2 / Add Charlie move    
 2023-02-01 / v0.0.3 / Add Charlie move and Animation    
+2023-02-01 / v0.0.4 / Add bgImage Copy    
